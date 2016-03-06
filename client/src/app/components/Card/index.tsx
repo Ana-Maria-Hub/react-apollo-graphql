53baf4157c5dcc537ccc6cbd1e388524f0588ab1
root <pearpaes@gmail.com>
2016-03-06 13:46:37 -0800

    index.tsx updated

--- client/src/app/components/Card/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Card as AntdCard } from 'antd';
-import styled from 'styled-components';
-import { animated } from 'react-spring';
-
-const Card = styled(animated(AntdCard))`
-  margin: 5rem !important;
-`;
-
-export default Card;
